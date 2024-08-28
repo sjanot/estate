afc765cff7397ae96d76cdd9f70dff10cef623a1
stef <sjanotak@gmail.com>
2024-08-28 21:35:55 +0200

    estate_account chapter 13 done

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, Command
 from odoo.exceptions import ValidationError
 
 
@@ -12,11 +12,13 @@ class Property(models.Model):
         new_account = self.env['account.move'].create({
             'partner_id': self.buyer.id,
             'move_type': 'out_invoice',
-            'journal_id': 1
+            'journal_id': 1,
+            'line_ids': [
+                Command.create({'name': self.name, 'quantity': 1,'price_unit': self.selling_price * 0.06 }),
+                Command.create({'name': 'administrative fees', 'quantity': 1, 'price_unit': 100 }),
+            ]
         })
-
         print("Sold property inherited")
-
         # Call the parent method to continue the standard process
         return super().sold_property()
 
