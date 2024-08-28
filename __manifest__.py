28fb0e802ad74cbf8508c9d04859f551f6c9689d
stef <sjanotak@gmail.com>
2024-08-28 08:50:35 +0200

    manifest corrected, updated

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,17 @@
 {
-    'name': 'Estate real',
+    'name': 'Real estate',
     'version': '17.0.0.0',
-    'summary': 'Estate real',
-    'description': 'Estate real',
-    'category': 'Category',
+    'summary': 'Real estate',
+    'description': 'Real estate',
     'author': 'Štefan Janoťák',
     'website': 'https://www.symetra.sk/',
     'license': 'LGPL-3',
     'application': True,
-    'depends': [],
+    'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ],
-    'demo': [],
     'installable': True,
 }
